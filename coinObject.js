2df103edee107bbbc061d87f0fd69d12cdeafb18
chris <cbw29512@outlook.com>
2019-09-28 13:43:59 -0400

    added flaming font and updated code

--- coinObject.js
+++ coinObject.js
@@ -1,14 +1,18 @@
+
 const coin = {
     state: 0,
     flip: function() {
-       this.state = Math.floor(Math.random() *2); 
+       this.state = Math.floor(Math.random() *2) 
+       return this.state
     },
+
     toString: function() {
-        if(this.state ===0)
-            return"Heads";
+        if(this.state === 0)
+            return"Heads"
         else
-            return "Tails";
+            return "Tails"
     },
+
     toHTML: function() {
         const image = document.createElement('img');
         if(this.state === 0)
@@ -17,17 +21,21 @@ const coin = {
             image.src = "images/yescoinNB.png"    
         return image;
     }
-};
+}
+
 function display20Flips() {
     const results = [];
     for (flips = 0; flips < 20; flips++) {
         coin.flip();
-        let transform = coin.toString();
-        results.push(transform);
-        document.write(transform + ", ");
+        let flipResult = document.createElement("span");
+        flipResult.textContent = coin.toString();
+        document.body.appendChild(flipResult);
+        document.body.appendChild(coin.toHTML());
     }
-    return results;
+    return results
 }
+
+
 function display20Images() {
     const results = [];
     for (display = 0; display < 20; display++) {
@@ -35,6 +43,9 @@ function display20Images() {
         let displayHtml = coin.toHTML();
         document.body.appendChild(displayHtml);
     }
+}
+function refreshPage() {
+    window.location.reload()
 }
 
 display20Flips();
